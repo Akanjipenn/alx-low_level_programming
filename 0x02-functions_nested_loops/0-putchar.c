60436c2d4930968a4e9c2e204fb805e504b9aad9
Yekinni Abdullahi Akanji <abupharm50@gmail.com>
2022-07-10 08:48:03 -0700

    Printed putchat

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,22 @@
-#includie "main.h"
+#include "main.h"
+
 /**
- * main ->Print _putchar
- * Return always 0
-*/
-int main(void)	
+ * main - function to print
+ *
+ * Return: 0
+ */
+int main(void)
 {
-	putchar('-');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
